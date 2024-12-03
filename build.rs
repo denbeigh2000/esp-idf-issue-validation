a90ddb5d73efe6683c52d5ec4a4cfb812aa3da09
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2024-12-03 07:50:30 +0000

    logging?

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+use std::env::current_dir;
+
 fn main() {
+    let metadata = cargo_metadata::MetadataCommand::new()
+        .current_dir(current_dir())
+        .other_options(vec!["--locked".into(), filter_string])
+        .exec()
+        .unwrap();
+    eprintln!("{:?}", metadata.root_package().unwrap().metadata);
     embuild::espidf::sysenv::output();
 }
